e549b69703c06c257bbefec13595e332af720295
Thomas Brinck <Thomas Brinck@192.168.0.13>
2018-05-28 11:44:21 +0200

    Begun writing README Updated Nexus endpoint for consistency and added redundancy for local execution

--- tools/architecture/src/main/java/net/thomas/portfolio/services/Service.java
+++ tools/architecture/src/main/java/net/thomas/portfolio/services/Service.java
@@ -3,12 +3,13 @@ package net.thomas.portfolio.services;
 import static net.thomas.portfolio.services.ServiceGlobals.ANALYTICS_SERVICE_PATH;
 import static net.thomas.portfolio.services.ServiceGlobals.HBASE_INDEXING_SERVICE_PATH;
 import static net.thomas.portfolio.services.ServiceGlobals.LEGAL_SERVICE_PATH;
+import static net.thomas.portfolio.services.ServiceGlobals.NEXUS_SERVICE_PATH;
 import static net.thomas.portfolio.services.ServiceGlobals.RENDER_SERVICE_PATH;
 import static net.thomas.portfolio.services.ServiceGlobals.USAGE_DATA_SERVICE_PATH;
 
 public enum Service implements ServiceEndpoint {
-	ANALYTICS_SERVICE(ANALYTICS_SERVICE_PATH), HBASE_INDEXING_SERVICE(HBASE_INDEXING_SERVICE_PATH), LEGAL_SERVICE(LEGAL_SERVICE_PATH), RENDER_SERVICE(
-			RENDER_SERVICE_PATH), USAGE_DATA_SERVICE(USAGE_DATA_SERVICE_PATH);
+	ANALYTICS_SERVICE(ANALYTICS_SERVICE_PATH), HBASE_INDEXING_SERVICE(HBASE_INDEXING_SERVICE_PATH), LEGAL_SERVICE(LEGAL_SERVICE_PATH), NEXUS_SERVICE(
+			NEXUS_SERVICE_PATH), RENDER_SERVICE(RENDER_SERVICE_PATH), USAGE_DATA_SERVICE(USAGE_DATA_SERVICE_PATH);
 
 	private final String path;
 
--- tools/architecture/src/main/java/net/thomas/portfolio/services/ServiceGlobals.java
+++ tools/architecture/src/main/java/net/thomas/portfolio/services/ServiceGlobals.java
@@ -2,8 +2,9 @@ package net.thomas.portfolio.services;
 
 public class ServiceGlobals {
 	public static final String ANALYTICS_SERVICE_PATH = "/AnalyticsService";
-	public static final String LEGAL_SERVICE_PATH = "/LegalService";
 	public static final String HBASE_INDEXING_SERVICE_PATH = "/HbaseIndexingService";
+	public static final String LEGAL_SERVICE_PATH = "/LegalService";
+	public static final String NEXUS_SERVICE_PATH = "/NexusService";
 	public static final String RENDER_SERVICE_PATH = "/RenderService";
 	public static final String USAGE_DATA_SERVICE_PATH = "/UsageDataService";
 }
