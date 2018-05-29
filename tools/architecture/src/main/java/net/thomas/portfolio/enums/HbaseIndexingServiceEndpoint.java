b84c38850f5c2bc2c37ce399a222d9d8b69c7502
Thomas Brinck <Thomas Brinck@192.168.0.13>
2018-05-29 19:45:32 +0200

    Added endpoint for looking up selector suggestions (content pending) Refactored some poorly chosen names

--- tools/architecture/src/main/java/net/thomas/portfolio/enums/HbaseIndexingServiceEndpoint.java
+++ tools/architecture/src/main/java/net/thomas/portfolio/enums/HbaseIndexingServiceEndpoint.java
@@ -4,17 +4,18 @@ import static net.thomas.portfolio.globals.HbaseIndexingServiceGlobals.GET_DATA_
 import static net.thomas.portfolio.globals.HbaseIndexingServiceGlobals.GET_REFERENCES_PATH;
 import static net.thomas.portfolio.globals.HbaseIndexingServiceGlobals.GET_SAMPLES_PATH;
 import static net.thomas.portfolio.globals.HbaseIndexingServiceGlobals.GET_SCHEMA_PATH;
+import static net.thomas.portfolio.globals.HbaseIndexingServiceGlobals.GET_SELECTOR_SUGGESTIONS_PATH;
 import static net.thomas.portfolio.globals.HbaseIndexingServiceGlobals.GET_STATISTICS_PATH;
 import static net.thomas.portfolio.globals.HbaseIndexingServiceGlobals.INVERTED_INDEX_LOOKUP_PATH;
 
 import net.thomas.portfolio.services.ServiceEndpoint;
 
-public enum HbaseDataServiceEndpoint implements ServiceEndpoint {
-	GET_SCHEMA(GET_SCHEMA_PATH), GET_SAMPLES(GET_SAMPLES_PATH), GET_DATA_TYPE(GET_DATA_TYPE_PATH), INVERTED_INDEX_LOOKUP(
-			INVERTED_INDEX_LOOKUP_PATH), GET_STATISTICS(GET_STATISTICS_PATH), GET_REFERENCES(GET_REFERENCES_PATH);
+public enum HbaseIndexingServiceEndpoint implements ServiceEndpoint {
+	GET_SCHEMA(GET_SCHEMA_PATH), GET_SAMPLES(GET_SAMPLES_PATH), GET_SELECTOR_SUGGESTIONS(GET_SELECTOR_SUGGESTIONS_PATH), GET_DATA_TYPE(
+			GET_DATA_TYPE_PATH), INVERTED_INDEX_LOOKUP(INVERTED_INDEX_LOOKUP_PATH), GET_STATISTICS(GET_STATISTICS_PATH), GET_REFERENCES(GET_REFERENCES_PATH);
 	private final String path;
 
-	private HbaseDataServiceEndpoint(String path) {
+	private HbaseIndexingServiceEndpoint(String path) {
 		this.path = path;
 	}
 
--- tools/architecture/src/main/java/net/thomas/portfolio/globals/HbaseIndexingServiceGlobals.java
+++ tools/architecture/src/main/java/net/thomas/portfolio/globals/HbaseIndexingServiceGlobals.java
@@ -5,6 +5,7 @@ public class HbaseIndexingServiceGlobals {
 	public static final String GET_DATA_TYPE_PATH = "/getDataType";
 	public static final String INVERTED_INDEX_LOOKUP_PATH = "/invertedIndexLookup";
 	public static final String GET_SAMPLES_PATH = "/getSamples";
+	public static final String GET_SELECTOR_SUGGESTIONS_PATH = "/getSelectorSuggestions";
 	public static final String GET_STATISTICS_PATH = "/getStatistics";
 	public static final String GET_REFERENCES_PATH = "/getReferences";
 }
