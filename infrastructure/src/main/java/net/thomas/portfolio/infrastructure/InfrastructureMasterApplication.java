a1c172e834ca3d918159f74a6a43e715461f2d2c
Thomas Brinck <Thomas Brinck@192.168.0.13>
2018-06-05 09:28:06 +0200

    Updated settings, including eureka renewal time

--- infrastructure/src/main/java/net/thomas/portfolio/infrastructure/InfrastructureMasterApplication.java
+++ infrastructure/src/main/java/net/thomas/portfolio/infrastructure/InfrastructureMasterApplication.java
@@ -1,14 +1,18 @@
 package net.thomas.portfolio.infrastructure;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
 @SpringBootApplication
 @EnableEurekaServer
-// @EnableHystrixDashboard
+@EnableAutoConfiguration
+@EnableAdminServer
 public class InfrastructureMasterApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(InfrastructureMasterApplication.class, args);
+		SpringApplication.run(InfrastructureMasterApplication.class);
 	}
 }
