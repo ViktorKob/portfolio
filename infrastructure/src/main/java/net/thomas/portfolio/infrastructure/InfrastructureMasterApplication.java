e6b84cfaf1d58450b44111d8c8e82fa350e5bf48
Thomas Brinck <Thomas Brinck@192.168.0.13>
2018-06-05 09:29:51 +0200

    Removed admin server again

--- infrastructure/src/main/java/net/thomas/portfolio/infrastructure/InfrastructureMasterApplication.java
+++ infrastructure/src/main/java/net/thomas/portfolio/infrastructure/InfrastructureMasterApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
-
 @SpringBootApplication
 @EnableEurekaServer
 @EnableAutoConfiguration
-@EnableAdminServer
+// @EnableAdminServer
 public class InfrastructureMasterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InfrastructureMasterApplication.class);
