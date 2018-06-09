807df635bf40ca9ae765d6b98f4f04c06d776e51
Thomas Brinck <Thomas Brinck@192.168.0.13>
2018-06-09 21:29:25 +0200

    Usage data storage
    Added storage calls to html and headline fetchers for when a document is
    shown

--- services/nexus-service/src/main/java/net/thomas/portfolio/nexus/graphql/fetchers/conversion/HtmlDataFetcher.java
+++ services/nexus-service/src/main/java/net/thomas/portfolio/nexus/graphql/fetchers/conversion/HtmlDataFetcher.java
@@ -1,10 +1,14 @@
 package net.thomas.portfolio.nexus.graphql.fetchers.conversion;
 
+import static net.thomas.portfolio.shared_objects.usage_data.UsageActivityType.READ_DOCUMENT;
+
 import graphql.schema.DataFetchingEnvironment;
 import net.thomas.portfolio.nexus.graphql.fetchers.ModelDataFetcher;
 import net.thomas.portfolio.shared_objects.adaptors.Adaptors;
 import net.thomas.portfolio.shared_objects.hbase_index.model.DataType;
+import net.thomas.portfolio.shared_objects.hbase_index.model.types.DataTypeId;
 import net.thomas.portfolio.shared_objects.hbase_index.model.types.DocumentInfo;
+import net.thomas.portfolio.shared_objects.usage_data.UsageActivity;
 
 public class HtmlDataFetcher extends ModelDataFetcher<String> {
 
@@ -16,9 +20,17 @@ public class HtmlDataFetcher extends ModelDataFetcher<String> {
 	public String _get(DataFetchingEnvironment environment) {
 		final Object entity = environment.getSource();
 		if (entity instanceof DataType) {
-			return adaptors.renderAsHtml(((DataType) entity).getId());
+			final DataTypeId id = ((DataType) entity).getId();
+			if (adaptors.isDocument(id.type)) {
+				adaptors.storeUsageActivity(id, new UsageActivity("Tester", READ_DOCUMENT, System.currentTimeMillis()));
+			}
+			return adaptors.renderAsHtml(id);
 		} else if (entity instanceof DocumentInfo) {
-			return adaptors.renderAsHtml(((DocumentInfo) entity).getId());
+			final DataTypeId id = ((DocumentInfo) entity).getId();
+			if (adaptors.isDocument(id.type)) {
+				adaptors.storeUsageActivity(id, new UsageActivity("Tester", READ_DOCUMENT, System.currentTimeMillis()));
+			}
+			return adaptors.renderAsHtml(id);
 		} else {
 			throw new RuntimeException("Unable to convert data type of type " + entity.getClass()
 				.getSimpleName());
