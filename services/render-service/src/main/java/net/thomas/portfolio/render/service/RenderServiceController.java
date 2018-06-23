ef1fd8c340a52d80b03a63feb5a42d8124391d1c
Thomas Brinck <Thomas Brinck@192.168.0.13>
2018-06-23 22:29:03 +0200

    Minor changes
    Made HBase schema loading lazy
    Updated README examples to reflect changes to GraphQL model

--- services/render-service/src/main/java/net/thomas/portfolio/render/service/RenderServiceController.java
+++ services/render-service/src/main/java/net/thomas/portfolio/render/service/RenderServiceController.java
@@ -5,8 +5,6 @@ import static net.thomas.portfolio.globals.RenderServiceGlobals.RENDER_AS_SIMPLE
 import static net.thomas.portfolio.globals.RenderServiceGlobals.RENDER_AS_TEXT_PATH;
 import static org.springframework.http.ResponseEntity.badRequest;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +16,11 @@ import org.springframework.web.client.RestTemplate;
 import com.netflix.discovery.EurekaClient;
 
 import net.thomas.portfolio.common.services.validation.SpecificStringPresenceValidator;
+import net.thomas.portfolio.render.common.context.HtmlRenderContext;
 import net.thomas.portfolio.render.common.context.HtmlRenderContextBuilder;
+import net.thomas.portfolio.render.common.context.SimpleRepresentationRenderContext;
 import net.thomas.portfolio.render.common.context.SimpleRepresentationRenderContextBuilder;
+import net.thomas.portfolio.render.common.context.TextRenderContext;
 import net.thomas.portfolio.render.common.context.TextRenderContextBuilder;
 import net.thomas.portfolio.render.format.html.HtmlRenderControl;
 import net.thomas.portfolio.render.format.simple_rep.HbaseIndexingModelSimpleRepresentationRendererLibrary;
@@ -27,6 +28,7 @@ import net.thomas.portfolio.render.format.text.HbaseIndexingModelTextRendererLib
 import net.thomas.portfolio.service_commons.services.HbaseIndexModelAdaptorImpl;
 import net.thomas.portfolio.service_commons.services.HttpRestClient;
 import net.thomas.portfolio.service_commons.validation.UidValidator;
+import net.thomas.portfolio.shared_objects.adaptors.HbaseIndexModelAdaptor;
 import net.thomas.portfolio.shared_objects.hbase_index.model.DataType;
 import net.thomas.portfolio.shared_objects.hbase_index.model.types.DataTypeId;
 
@@ -41,7 +43,7 @@ public class RenderServiceController {
 	private final HtmlRenderControl htmlRenderer;
 	@Autowired
 	private EurekaClient discoveryClient;
-	private HbaseIndexModelAdaptorImpl hbaseIndexAdaptor;
+	private HbaseIndexModelAdaptorImpl hbaseAdaptor;
 
 	public RenderServiceController(RenderServiceConfiguration config) {
 		this.config = config;
@@ -50,12 +52,12 @@ public class RenderServiceController {
 		htmlRenderer = new HtmlRenderControl();
 	}
 
-	@PostConstruct
-	public void prepareForRendering() {
-		new Thread(() -> {
-			hbaseIndexAdaptor = new HbaseIndexModelAdaptorImpl(new HttpRestClient(discoveryClient, getRestTemplate(), config.getHbaseIndexing()));
-			TYPE.setValidStrings(hbaseIndexAdaptor.getDataTypes());
-		}).run();
+	public synchronized HbaseIndexModelAdaptor getHbaseAdaptor() {
+		if (hbaseAdaptor == null) {
+			hbaseAdaptor = new HbaseIndexModelAdaptorImpl(new HttpRestClient(discoveryClient, getRestTemplate(), config.getHbaseIndexing()));
+			TYPE.setValidStrings(hbaseAdaptor.getDataTypes());
+		}
+		return hbaseAdaptor;
 	}
 
 	@Bean
@@ -68,10 +70,11 @@ public class RenderServiceController {
 	@RequestMapping(RENDER_AS_SIMPLE_REPRESENTATION_PATH)
 	public ResponseEntity<String> renderAsSimpleRepresentation(DataTypeId id) {
 		if (TYPE.isValid(id.type) && UID.isValid(id.uid)) {
-			final DataType entity = hbaseIndexAdaptor.getDataType(id);
+			final DataType entity = getHbaseAdaptor().getDataType(id);
 			if (entity != null) {
-				return ResponseEntity.ok(simpleRepRenderer.render(entity, new SimpleRepresentationRenderContextBuilder().setHbaseModelAdaptor(hbaseIndexAdaptor)
-					.build()));
+				final SimpleRepresentationRenderContext renderContext = new SimpleRepresentationRenderContextBuilder().setHbaseModelAdaptor(getHbaseAdaptor())
+					.build();
+				return ResponseEntity.ok(simpleRepRenderer.render(entity, renderContext));
 			} else {
 				return ResponseEntity.notFound()
 					.build();
@@ -85,9 +88,10 @@ public class RenderServiceController {
 	@RequestMapping(RENDER_AS_TEXT_PATH)
 	public ResponseEntity<String> renderAsText(DataTypeId id) {
 		if (TYPE.isValid(id.type) && UID.isValid(id.uid)) {
-			final DataType entity = hbaseIndexAdaptor.getDataType(id);
+			final DataType entity = getHbaseAdaptor().getDataType(id);
 			if (entity != null) {
-				return ResponseEntity.ok(textRenderer.render(entity, new TextRenderContextBuilder().build()));
+				final TextRenderContext renderContext = new TextRenderContextBuilder().build();
+				return ResponseEntity.ok(textRenderer.render(entity, renderContext));
 			} else {
 				return ResponseEntity.notFound()
 					.build();
@@ -101,9 +105,10 @@ public class RenderServiceController {
 	@RequestMapping(RENDER_AS_HTML_PATH)
 	public ResponseEntity<String> renderAsHtml(DataTypeId id) {
 		if (TYPE.isValid(id.type) && UID.isValid(id.uid)) {
-			final DataType entity = hbaseIndexAdaptor.getDataType(id);
+			final DataType entity = getHbaseAdaptor().getDataType(id);
 			if (entity != null) {
-				return ResponseEntity.ok(htmlRenderer.render(entity, new HtmlRenderContextBuilder().build()));
+				final HtmlRenderContext renderContext = new HtmlRenderContextBuilder().build();
+				return ResponseEntity.ok(htmlRenderer.render(entity, renderContext));
 			} else {
 				return ResponseEntity.notFound()
 					.build();
