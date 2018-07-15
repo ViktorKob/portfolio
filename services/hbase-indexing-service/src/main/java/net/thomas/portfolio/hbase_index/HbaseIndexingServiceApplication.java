17c3599c2cd31995af755a666cfc09db1db30a4a
Thomas Brinck <Thomas Brinck@192.168.0.13>
2018-07-15 13:23:39 +0200

    Added CSRF disable to HbaseIndexing as well to allow it to be accessed

--- services/hbase-indexing-service/src/main/java/net/thomas/portfolio/hbase_index/HbaseIndexingServiceApplication.java
+++ services/hbase-indexing-service/src/main/java/net/thomas/portfolio/hbase_index/HbaseIndexingServiceApplication.java
@@ -6,12 +6,29 @@ import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class HbaseIndexingServiceApplication {
+	@Configuration
+	static class CsrfBugWorkaround extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf()
+				.disable()
+				.authorizeRequests()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.httpBasic();
+		}
+	}
+
 	public static void main(String[] args) {
 		setProperty("server.servlet.context-path", HBASE_INDEXING_SERVICE_PATH);
 		run(HbaseIndexingServiceApplication.class, args);
