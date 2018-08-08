242192f27251e2e683296f259584c9912c9b405a
Thomas Brinck <Thomas Brinck@192.168.0.11>
2018-08-08 19:49:45 +0200

    Added EventGeneratorUnitTest

--- services/hbase-indexing-service/src/test/java/net/thomas/portfolio/hbase_index/fake/generators/GeneratorTestSuite.java
+++ services/hbase-indexing-service/src/test/java/net/thomas/portfolio/hbase_index/fake/generators/GeneratorTestSuite.java
@@ -6,6 +6,6 @@ import org.junit.runners.Suite;
 import net.thomas.portfolio.hbase_index.fake.generators.selectors.SelectorGeneratorTestSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ EntityGeneratorUnitTest.class, SelectorGeneratorTestSuite.class })
+@Suite.SuiteClasses({ EntityGeneratorUnitTest.class, EventGeneratorUnitTest.class, SelectorGeneratorTestSuite.class })
 public class GeneratorTestSuite {
 }
