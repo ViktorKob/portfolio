24ca5ee42128ebe825f5c4d2fdea5e95864438d7
Thomas Brinck <Thomas Brinck@192.168.0.11>
2018-08-09 22:20:54 +0200

    Added unit tests for simple representation renderer library

--- services/render-service/src/test/java/net/thomas/portfolio/render/RenderServiceTestSuite.java
+++ services/render-service/src/test/java/net/thomas/portfolio/render/RenderServiceTestSuite.java
@@ -3,10 +3,12 @@ package net.thomas.portfolio.render;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import net.thomas.portfolio.render.format.simple_rep.HbaseIndexingModelSimpleRepresentationRendererLibraryUnitTest;
 import net.thomas.portfolio.render.format.text.HbaseIndexingModelTextRendererLibraryUnitTest;
 import net.thomas.portfolio.render.service.RenderServiceControllerServiceAdaptorTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ HbaseIndexingModelTextRendererLibraryUnitTest.class, RenderServiceControllerServiceAdaptorTest.class })
+@Suite.SuiteClasses({ HbaseIndexingModelTextRendererLibraryUnitTest.class, HbaseIndexingModelSimpleRepresentationRendererLibraryUnitTest.class,
+		RenderServiceControllerServiceAdaptorTest.class })
 public class RenderServiceTestSuite {
 }
