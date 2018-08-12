fc1339cd72a928ef7bc42fd68dba4814fe41a357
Thomas Brinck <Thomas Brinck@192.168.0.11>
2018-08-12 17:47:46 +0200

    Added inverted index lookup and builder tests

--- services/hbase-indexing-service/src/test/java/net/thomas/portfolio/hbase_index/HbaseIndexingServiceTestSuite.java
+++ services/hbase-indexing-service/src/test/java/net/thomas/portfolio/hbase_index/HbaseIndexingServiceTestSuite.java
@@ -5,10 +5,12 @@ import org.junit.runners.Suite;
 
 import net.thomas.portfolio.hbase_index.fake.FakeHbaseIndexUnitTest;
 import net.thomas.portfolio.hbase_index.fake.generators.GeneratorTestSuite;
+import net.thomas.portfolio.hbase_index.lookup.InvertedIndexLookupAndBuilderUnitTest;
 import net.thomas.portfolio.hbase_index.schema.SchemaTestSuite;
 import net.thomas.portfolio.hbase_index.service.HbaseIndexingServiceControllerServiceAdaptorTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ FakeHbaseIndexUnitTest.class, SchemaTestSuite.class, GeneratorTestSuite.class, HbaseIndexingServiceControllerServiceAdaptorTest.class })
+@Suite.SuiteClasses({ FakeHbaseIndexUnitTest.class, SchemaTestSuite.class, GeneratorTestSuite.class, HbaseIndexingServiceControllerServiceAdaptorTest.class,
+		InvertedIndexLookupAndBuilderUnitTest.class })
 public class HbaseIndexingServiceTestSuite {
 }
