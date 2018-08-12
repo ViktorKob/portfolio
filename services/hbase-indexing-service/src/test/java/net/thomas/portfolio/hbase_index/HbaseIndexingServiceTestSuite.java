928ee518c869bca4325ad75c78927ba3aa8e739e
Thomas Brinck <Thomas Brinck@192.168.0.11>
2018-08-12 18:46:15 +0200

    Added tests for fake processing steps

--- services/hbase-indexing-service/src/test/java/net/thomas/portfolio/hbase_index/HbaseIndexingServiceTestSuite.java
+++ services/hbase-indexing-service/src/test/java/net/thomas/portfolio/hbase_index/HbaseIndexingServiceTestSuite.java
@@ -3,14 +3,13 @@ package net.thomas.portfolio.hbase_index;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import net.thomas.portfolio.hbase_index.fake.FakeHbaseIndexUnitTest;
-import net.thomas.portfolio.hbase_index.fake.generators.GeneratorTestSuite;
+import net.thomas.portfolio.hbase_index.fake.FakeComponentsTestSuite;
 import net.thomas.portfolio.hbase_index.lookup.InvertedIndexLookupAndBuilderUnitTest;
 import net.thomas.portfolio.hbase_index.schema.SchemaTestSuite;
 import net.thomas.portfolio.hbase_index.service.HbaseIndexingServiceControllerServiceAdaptorTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ FakeHbaseIndexUnitTest.class, SchemaTestSuite.class, GeneratorTestSuite.class, HbaseIndexingServiceControllerServiceAdaptorTest.class,
+@Suite.SuiteClasses({ FakeComponentsTestSuite.class, SchemaTestSuite.class, HbaseIndexingServiceControllerServiceAdaptorTest.class,
 		InvertedIndexLookupAndBuilderUnitTest.class })
 public class HbaseIndexingServiceTestSuite {
 }
