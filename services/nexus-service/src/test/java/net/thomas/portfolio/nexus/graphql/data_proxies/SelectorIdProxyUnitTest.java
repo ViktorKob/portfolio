89a0ac3214167943939b50069fc25de02063ad91
Thomas Brinck <Thomas Brinck@192.168.0.11>
2018-08-14 22:07:31 +0200

    Added tests for GraphQL data type proxies

--- services/nexus-service/src/test/java/net/thomas/portfolio/nexus/graphql/data_proxies/SelectorIdProxyUnitTest.java
+++ services/nexus-service/src/test/java/net/thomas/portfolio/nexus/graphql/data_proxies/SelectorIdProxyUnitTest.java
@@ -1,20 +1,25 @@
 package net.thomas.portfolio.nexus.graphql.data_proxies;
 
 import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import net.thomas.portfolio.service_commons.adaptors.Adaptors;
-import net.thomas.portfolio.shared_objects.hbase_index.model.types.DataType;
 import net.thomas.portfolio.shared_objects.hbase_index.model.types.DataTypeId;
 
-public class DataTypeEntityProxyUnitTest {
-	private DataTypeEntityProxy proxy;
+public class SelectorIdProxyUnitTest {
+	private SelectorIdProxy proxy;
+	private Adaptors adaptors;
 
 	@Before
 	public void setUpForTest() {
-		proxy = new DataTypeEntityProxy(SOME_ENTITY, ADAPTORS_ARE_IGNORED);
+		adaptors = mock(Adaptors.class);
+		proxy = new SelectorIdProxy(SOME_ID, adaptors);
 	}
 
 	@Test
@@ -24,17 +29,17 @@ public class DataTypeEntityProxyUnitTest {
 
 	@Test
 	public void shouldContainEntity() {
-		assertSame(SOME_ENTITY, proxy.getEntity());
+		proxy.getEntity();
+		verify(adaptors, times(1)).getDataType(same(SOME_ID));
 	}
 
 	@Test
 	public void shouldWorkWithParentBasedConstructor() {
-		proxy = new DataTypeEntityProxy(proxy, SOME_ENTITY, ADAPTORS_ARE_IGNORED);
+		proxy = new SelectorIdProxy(proxy, SOME_ID, adaptors);
 		assertSame(SOME_ID, proxy.getId());
-		assertSame(SOME_ENTITY, proxy.getEntity());
+		proxy.getEntity();
+		verify(adaptors, times(1)).getDataType(same(SOME_ID));
 	}
 
 	private static final DataTypeId SOME_ID = new DataTypeId("SomeType", "AA01");
-	private final DataType SOME_ENTITY = new DataType(SOME_ID);
-	private static final Adaptors ADAPTORS_ARE_IGNORED = null;
 }
