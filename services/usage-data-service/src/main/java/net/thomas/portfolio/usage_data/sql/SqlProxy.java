37dd5775d96e590e477e01d1c108df33a7f84516
Thomas <Thomas@192.168.0.17>
2018-08-19 10:55:53 +0200

    Version cleanup and preparation for migration to Java 10

--- services/usage-data-service/src/main/java/net/thomas/portfolio/usage_data/sql/SqlProxy.java
+++ services/usage-data-service/src/main/java/net/thomas/portfolio/usage_data/sql/SqlProxy.java
@@ -41,7 +41,7 @@ public class SqlProxy {
 	private static final boolean WITH_SCHEMA = true;
 	private Database databaseConfig;
 
-	public void setDatabase(Database databaseConfig) {
+	public void setDatabase(final Database databaseConfig) {
 		this.databaseConfig = databaseConfig;
 	}
 
@@ -59,25 +59,25 @@ public class SqlProxy {
 		}
 	}
 
-	private boolean checkForDatabase(Connection connection) throws SQLException {
+	private boolean checkForDatabase(final Connection connection) throws SQLException {
 		boolean databaseIsPresent = false;
 		try (Statement checkForDatabase = connection.createStatement()) {
-			checkForDatabase.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '" + databaseConfig.getSchema() + "'");
+			checkForDatabase.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '"
+					+ databaseConfig.getSchema() + "'");
 			final ResultSet results = checkForDatabase.getResultSet();
-			databaseIsPresent = results.first() && databaseConfig.getSchema()
-				.equals(results.getString("SCHEMA_NAME"));
+			databaseIsPresent = results.first() && databaseConfig.getSchema().equals(results.getString("SCHEMA_NAME"));
 		}
 		return databaseIsPresent;
 	}
 
-	private void createAndInitializeDatabase(Connection connection) throws SQLException {
+	private void createAndInitializeDatabase(final Connection connection) throws SQLException {
 		try (Statement statement = connection.createStatement()) {
 			statement.execute("CREATE DATABASE " + databaseConfig.getSchema());
 			statement.execute("USE " + databaseConfig.getSchema());
 			final Path schemaPath = determineSchemaPath();
-			try (final BufferedReader reader = new BufferedReader(new InputStreamReader(Files.newInputStream(schemaPath, READ)))) {
-				final String sql = reader.lines()
-					.collect(Collectors.joining(" "));
+			try (final BufferedReader reader = new BufferedReader(
+					new InputStreamReader(Files.newInputStream(schemaPath, READ)))) {
+				final String sql = reader.lines().collect(Collectors.joining(" "));
 				for (final String sqlStatement : sql.split(";")) {
 					statement.execute(sqlStatement);
 				}
@@ -89,15 +89,14 @@ public class SqlProxy {
 
 	private Path determineSchemaPath() {
 		Path schemaPath = Paths.get("schema", databaseConfig.getSchema() + "_schema.sql");
-		if (!schemaPath.toFile()
-			.exists()) {
+		if (!schemaPath.toFile().exists()) {
 			schemaPath = Paths.get(".", "src", "main", "resources", "schema", "usage_data_schema.sql");
 		}
 		out.println("Using schema path " + schemaPath);
 		return schemaPath;
 	}
 
-	public void storeUsageActivity(DataTypeId id, UsageActivity activity) {
+	public void storeUsageActivity(final DataTypeId id, final UsageActivity activity) {
 		if (activity.timeOfActivity == null) {
 			activity.timeOfActivity = currentTimeMillis();
 		}
@@ -107,66 +106,48 @@ public class SqlProxy {
 				final UInteger userId = addOrGetUser(activity.user, configuration);
 				final UInteger accessTypeId = addOrGetAccessType(activity.type, configuration);
 				DSL.using(configuration)
-					.insertInto(USER_ACCESSED_DOCUMENT, USER_ACCESSED_DOCUMENT.DOCUMENT_TYPE, USER_ACCESSED_DOCUMENT.DOCUMENT_UID,
-							USER_ACCESSED_DOCUMENT.USER_ID, USER_ACCESSED_DOCUMENT.ACCESS_TYPE_ID, USER_ACCESSED_DOCUMENT.TIME_OF_ACCESS)
-					.values(id.type, id.uid, userId, accessTypeId, new Timestamp(activity.timeOfActivity))
-					.execute();
+						.insertInto(USER_ACCESSED_DOCUMENT, USER_ACCESSED_DOCUMENT.DOCUMENT_TYPE,
+								USER_ACCESSED_DOCUMENT.DOCUMENT_UID, USER_ACCESSED_DOCUMENT.USER_ID,
+								USER_ACCESSED_DOCUMENT.ACCESS_TYPE_ID, USER_ACCESSED_DOCUMENT.TIME_OF_ACCESS)
+						.values(id.type, id.uid, userId, accessTypeId, new Timestamp(activity.timeOfActivity))
+						.execute();
 			});
 		} catch (final SQLException e) {
 			throw new RuntimeException("Unable to fetch activity from database", e);
 		}
 	}
 
-	private UInteger addOrGetUser(String username, Configuration configuration) {
-		DSL.using(configuration)
-			.insertInto(USER, USER.NAME)
-			.values(username)
-			.onDuplicateKeyIgnore()
-			.execute();
-		return DSL.using(configuration)
-			.select(USER.ID)
-			.from(USER)
-			.where(USER.NAME.eq(username))
-			.fetchOne()
-			.get(USER.ID);
+	private UInteger addOrGetUser(final String username, final Configuration configuration) {
+		DSL.using(configuration).insertInto(USER, USER.NAME).values(username).onDuplicateKeyIgnore().execute();
+		return DSL.using(configuration).select(USER.ID).from(USER).where(USER.NAME.eq(username)).fetchOne()
+				.get(USER.ID);
 	}
 
-	private UInteger addOrGetAccessType(UsageActivityType accessType, Configuration configuration) {
-		DSL.using(configuration)
-			.insertInto(ACCESS_TYPE, ACCESS_TYPE.NAME)
-			.values(accessType.name())
-			.onDuplicateKeyIgnore()
-			.execute();
-		return DSL.using(configuration)
-			.select(ACCESS_TYPE.ID)
-			.from(ACCESS_TYPE)
-			.where(ACCESS_TYPE.NAME.eq(accessType.name()))
-			.fetchOne()
-			.get(ACCESS_TYPE.ID);
+	private UInteger addOrGetAccessType(final UsageActivityType accessType, final Configuration configuration) {
+		DSL.using(configuration).insertInto(ACCESS_TYPE, ACCESS_TYPE.NAME).values(accessType.name())
+				.onDuplicateKeyIgnore().execute();
+		return DSL.using(configuration).select(ACCESS_TYPE.ID).from(ACCESS_TYPE)
+				.where(ACCESS_TYPE.NAME.eq(accessType.name())).fetchOne().get(ACCESS_TYPE.ID);
 	}
 
-	public UsageActivities fetchUsageActivities(DataTypeId id, Bounds bounds) {
+	public UsageActivities fetchUsageActivities(final DataTypeId id, final Bounds bounds) {
 		try (Connection connection = createConnection(WITH_SCHEMA)) {
 			final DSLContext create = DSL.using(connection);
-			final Result<Record3<String, String, Timestamp>> result = create.select(USER.NAME, ACCESS_TYPE.NAME, USER_ACCESSED_DOCUMENT.TIME_OF_ACCESS)
-				.from(USER_ACCESSED_DOCUMENT)
-				.join(USER)
-				.on(USER_ACCESSED_DOCUMENT.USER_ID.eq(USER.ID))
-				.join(ACCESS_TYPE)
-				.on(USER_ACCESSED_DOCUMENT.ACCESS_TYPE_ID.eq(ACCESS_TYPE.ID))
-				.where(USER_ACCESSED_DOCUMENT.DOCUMENT_TYPE.eq(id.type))
-				.and(USER_ACCESSED_DOCUMENT.DOCUMENT_UID.eq(id.uid))
-				.and(USER_ACCESSED_DOCUMENT.TIME_OF_ACCESS.between(new Timestamp(bounds.after), new Timestamp(bounds.before)))
-				.orderBy(USER_ACCESSED_DOCUMENT.TIME_OF_ACCESS.desc())
-				.offset(bounds.offset)
-				.limit(bounds.limit)
-				.fetch();
+			final Result<Record3<String, String, Timestamp>> result = create
+					.select(USER.NAME, ACCESS_TYPE.NAME, USER_ACCESSED_DOCUMENT.TIME_OF_ACCESS)
+					.from(USER_ACCESSED_DOCUMENT).join(USER).on(USER_ACCESSED_DOCUMENT.USER_ID.eq(USER.ID))
+					.join(ACCESS_TYPE).on(USER_ACCESSED_DOCUMENT.ACCESS_TYPE_ID.eq(ACCESS_TYPE.ID))
+					.where(USER_ACCESSED_DOCUMENT.DOCUMENT_TYPE.eq(id.type))
+					.and(USER_ACCESSED_DOCUMENT.DOCUMENT_UID.eq(id.uid))
+					.and(USER_ACCESSED_DOCUMENT.TIME_OF_ACCESS.between(new Timestamp(bounds.after),
+							new Timestamp(bounds.before)))
+					.orderBy(USER_ACCESSED_DOCUMENT.TIME_OF_ACCESS.desc()).offset(bounds.offset).limit(bounds.limit)
+					.fetch();
 			final LinkedList<UsageActivity> activities = new LinkedList<>();
 			for (final Record3<String, String, Timestamp> activity : result) {
 				final String username = activity.get(USER.NAME);
 				final UsageActivityType activityType = UsageActivityType.valueOf(activity.get(ACCESS_TYPE.NAME));
-				final long timeOfActivity = activity.get(USER_ACCESSED_DOCUMENT.TIME_OF_ACCESS)
-					.getTime();
+				final long timeOfActivity = activity.get(USER_ACCESSED_DOCUMENT.TIME_OF_ACCESS).getTime();
 				activities.add(new UsageActivity(username, activityType, timeOfActivity));
 			}
 			return new UsageActivities(activities);
@@ -175,7 +156,8 @@ public class SqlProxy {
 		}
 	}
 
-	private Connection createConnection(boolean withSchema) throws SQLException {
-		return getConnection(databaseConfig.getConnectionString(withSchema), databaseConfig.getUser(), databaseConfig.getPassword());
+	private Connection createConnection(final boolean withSchema) throws SQLException {
+		return getConnection(databaseConfig.getConnectionString(withSchema), databaseConfig.getUser(),
+				databaseConfig.getPassword());
 	}
 }
