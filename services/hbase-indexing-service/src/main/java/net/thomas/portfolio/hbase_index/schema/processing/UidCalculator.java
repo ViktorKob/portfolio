e23592c815e3cda2bf83d8b0ae5f3a33190c5693
Thomas Brinck <Thomas Brinck@192.168.0.13>
2018-12-17 20:25:13 +0100

    Yet another codacy attempt

--- services/hbase-indexing-service/src/main/java/net/thomas/portfolio/hbase_index/schema/processing/UidCalculator.java
+++ services/hbase-indexing-service/src/main/java/net/thomas/portfolio/hbase_index/schema/processing/UidCalculator.java
@@ -52,7 +52,6 @@ public class UidCalculator {
 			for (final Object listEntity : (Object[]) field.get(entity)) {
 				hasher.add(interpret(field.get(listEntity)));
 			}
-
 		} else {
 			hasher.add(interpret(field.get(entity)));
 		}
