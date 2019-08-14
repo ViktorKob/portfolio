3aff504c64341a679fc6c602069be32a2ece2b6e
Thomas Brinck <Not@shared.dk>
2019-08-14 22:52:53 +0200

    Added Lombok example to test the build server with the project

---
+++ tools/common/src/main/java/net/thomas/portfolio/common/utils/LombokTest.java
@@ -0,0 +1,19 @@
+package net.thomas.portfolio.common.utils;
+
+import lombok.Data;
+
+public class LombokTest {
+	public static void main(String[] args) {
+		final TestObject left = new TestObject(1);
+		final TestObject right = new TestObject(1);
+		final TestObject other = new TestObject(2);
+		System.out.println(left);
+		System.out.println(left.equals(right));
+		System.out.println(left.equals(other));
+	}
+}
+
+@Data
+class TestObject {
+	private final int value;
+}
