4235d4102e11b32f25ea79d262f5464799b14702
Thomas Brinck <Not@shared.dk>
2019-08-16 15:25:28 +0200

    Begun adding web content to Legal Service
    - Added Eclipse resource filter for data folders in HbaseIndexService
    - Added Eclipse resource filter for npm folders
    - Added basic javascript builder using npm and webpack
    - Added simple javascript code as example
    - Updated .gitignore with npm and node.js folders

---
+++ services/legal-service/src/main/web/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path')
+var webpack = require('webpack')
+    
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    path: path.resolve(__dirname, '../../../target/web'),
+    publicPath: '/vue-components/dist/', filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: [ 'vue-style-loader', 'css-loader' ] },
+      { test: /\.vue$/, loader: 'vue-loader', options: { loaders: {} } },
+      { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } },
+      {
+          test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+          use: [{ loader: 'url-loader', options: { limit: 10240 } }]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }, extensions: ['*', '.js', '.vue', '.json']
+  }
+};
+    
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.optimize.UglifyJsPlugin({})])}
