1d5303c56619c6c9a2b0e5ff3a3701e95280ea3c
Thomas Brinck <Not@shared.dk>
2019-08-19 23:45:59 +0200

    Working on JavaScript setup for Legal Service

--- services/legal-service/src/main/web/webpack.config.js
+++ services/legal-service/src/main/web/webpack.config.js
@@ -1,30 +1,16 @@
-var path = require("path");
-var webpack = require("webpack");
-    
+var packageJSON = require('./package.json');
+var path = require('path');
+var webpack = require('webpack');
+
+const PATHS = {
+  build: path.join(__dirname, '..','..','..', 'target', 'classes', 'META-INF', 'resources', 'webjars', packageJSON.name, packageJSON.version)
+};
+
 module.exports = {
-  entry: "./app/main.js",
+  entry: './app/main.js',
+
   output: {
-    path: path.resolve(__dirname, "../../../target/web"),
-    publicPath: "/vue-components/dist/", filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      { test: /\.css$/, use: [ "vue-style-loader", "css-loader" ] },
-      { test: /\.vue$/, loader: "vue-loader", options: { loaders: {} } },
-      { test: /\.js$/, exclude: /node_modules/, use: { loader: "babel-loader" } },
-      {
-          test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-          use: [{ loader: "url-loader", options: { limit: 10240 } }]
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      "vue$": "vue/dist/vue.esm.js"
-    }, extensions: ["*", ".js", ".vue", ".json"]
+    path: PATHS.build,
+    filename: 'legal_service_main.js'
   }
 };
-    
-if (process.env.NODE_ENV === "production") {
-    module.exports.plugins = (module.exports.plugins || []).concat([new webpack.optimize.UglifyJsPlugin({})]);
-}
