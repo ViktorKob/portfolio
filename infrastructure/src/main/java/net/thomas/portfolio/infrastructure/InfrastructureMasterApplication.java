6ecb8dbd6f1b37a959b70ffde9200c5d70e9b720
Thomas Brinck <Not@shared.dk>
2019-11-19 16:35:32 +0100

    Added workaround for missing bean declaration after Java update to 11

--- infrastructure/src/main/java/net/thomas/portfolio/infrastructure/InfrastructureMasterApplication.java
+++ infrastructure/src/main/java/net/thomas/portfolio/infrastructure/InfrastructureMasterApplication.java
@@ -4,10 +4,14 @@ import static net.thomas.portfolio.services.Service.loadServicePathsIntoProperti
 import static net.thomas.portfolio.services.configuration.InfrastructureServiceProperties.loadInfrastructureServiceConfigurationIntoProperties;
 import static org.springframework.boot.SpringApplication.run;
 
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +29,12 @@ public class InfrastructureMasterApplication {
 		protected void configure(HttpSecurity http) throws Exception {
 			http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
 		}
+	}
+
+	@ConditionalOnMissingBean
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 	public static void main(String[] args) {
